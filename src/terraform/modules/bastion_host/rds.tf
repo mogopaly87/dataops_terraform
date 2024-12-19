88c6aba7085fa89c164de02c1784bfb55b63ec82
Chinonso Mogo <mogononso@gmail.com>
2024-12-19 14:31:56 -0700

    updated rds file

--- src/terraform/modules/bastion_host/rds.tf
+++ src/terraform/modules/bastion_host/rds.tf
@@ -7,7 +7,7 @@ resource "random_id" "master_password" {
 # subnets of AZ A and B
 resource "aws_db_subnet_group" "database" {
   name       = "${var.project}-db-subnet-group"
-  subnet_ids = [data.aws_subnet.private_subnet_a.id, data.aws_subnet.private_subnet_b_id]
+  subnet_ids = [data.aws_subnet.private_subnet_a_id, data.aws_subnet.private_subnet_b_id]
 }
 
 # Complete the configuration for the RDS instance
@@ -16,16 +16,16 @@ resource "aws_db_instance" "database" {
   instance_class         = "db.t3.micro" # Use the db.t3.micro instance type
   allocated_storage      = 20
   storage_type           = "gp2"
-  db_subnet_group_name   = data.aws_db_subnet_group.database.name # Use the db subnet group you created above
-  vpc_security_group_ids = [data.aws_security_group.database.id]  # Use the security group you created for the RDS in network.tf
+  db_subnet_group_name   = aws_db_subnet_group.database.name # Use the db subnet group you created above
+  vpc_security_group_ids = [aws_security_group.database.id]  # Use the security group you created for the RDS in network.tf
 
   engine         = "postgres"
   engine_version = "15"
 
   port     = 5432
   db_name  = "postgres"
-  username = var.db_master_username            # Use the master username variable
-  password = data.random_id.master_password.id # Use the master password generated above
+  username = var.db_master_username       # Use the master username variable
+  password = random_id.master_password.id # Use the master password generated above
 
   publicly_accessible = false
   skip_final_snapshot = true
